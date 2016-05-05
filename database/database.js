efa6bd51fb8423b825b639006150930fbb73ab6b
Harman Singh <harman.singh@hotmail.co.nz>
2016-05-05 21:16:38 +1200

    added total cost and customer cost to mail table

--- database/database.js
+++ database/database.js
@@ -25,16 +25,17 @@ db.serialize(function(){
             + 'name TEXT'
             + ')');
 
-            db.run('CREATE TABLE IF NOT EXISTS companies ('
-                + 'companyid INTEGER PRIMARY KEY, '
-                + 'name TEXT, '
-                + 'type TEXT'
-                + ')');
+        db.run('CREATE TABLE IF NOT EXISTS companies ('
+            + 'companyid INTEGER PRIMARY KEY, '
+            + 'name TEXT, '
+            + 'type TEXT'
+            + ')');
 
         db.run('CREATE TABLE IF NOT EXISTS routes ('
             + 'routeid INTEGER PRIMARY KEY, '
             + 'company INTEGER, '
-            + 'location INTEGER, '
+            + 'origin INTEGER, '
+            + 'destination INTEGER'
             + 'weightcost INTEGER, '
             + 'volumecost INTEGER, '
             + 'maxweight INTEGER, '
@@ -42,7 +43,8 @@ db.serialize(function(){
             + 'duration INTEGER, '
             + 'day INTEGER, ' //day of the week will be represented as an integer 0 <= day < 7
             + 'FOREIGN KEY(company) REFERENCES companies(companyid), '
-            + 'FOREIGN KEY(location) REFERENCES locations(locationid) '
+            + 'FOREIGN KEY(origin) REFERENCES locations(origin), '
+            + 'FOREIGN KEY(destination) REFERENCES locations(destination) '
             + ')');
 
         db.run('CREATE TABLE IF NOT EXISTS mails ('
@@ -52,6 +54,8 @@ db.serialize(function(){
             + 'weight INTEGER, '
             + 'volume INTEGER, '
             + 'priority TEXT, '
+            + 'totalcustomercost REAL, '
+            + 'totalbusinesscost REAL, '
             + 'date TEXT, ' //storing date as string since it is simplest to work with javascript date object
             + 'FOREIGN KEY(origin) REFERENCES locations(locationid), '
             + 'FOREIGN KEY(destination) REFERENCES locations(locationid) '
@@ -83,6 +87,30 @@ this.getCustomerPrice = function(toLocation, fromLocation, priority, callback){
 
 function Database(){
     "use strict"
+    /*
+    Functions to implement:
+     1. get locations ==> returns array of location objects
+     1.1 get location based on name
+     1.2 get location based on id
+     1.3 insert location
+     2. get mails ==> returns array of mail
+     2.1 get mail based on id
+     2.2 get mail between dates
+     2.3 get mail on date
+     2.4 insert mail
+     3. get companies ==> returns array of companies
+     3.1 get company by name
+     3.2 get company by id
+     3.3 insert company
+     4. get routes ==> returns array of routes
+     4.1 get route of origin and destination
+     4.2 get route by id
+     4.3 insert route
+     5. get list of customer prices ==> returns array of customer prices
+     5.1 get customer price by id
+     5.2 get customer price for given origin and destination
+     5.3 insert customer price
+    */
 
 }
 
