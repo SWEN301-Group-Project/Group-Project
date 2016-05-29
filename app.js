c50aedd352afe05e77df1be0ee84bd07d75c9d96
Elliot Wilde <elliotw@hotmail.com>
2016-05-29 22:21:53 +1200

    Route/Price update forms autopopulate fields, also adds to database

--- app.js
+++ app.js
@@ -33,7 +33,9 @@ var express = require('express'),
     //following are used to interact with the database.
     Company = require('./database/company'),
     Mail = require('./database/mail'),
-    Location = require('./database/location');
+    Location = require('./database/location'),
+    Route = require('./database/routes'),
+    Price = require('./database/customerprice'),
 
     Graph = require('./database/graph');
 
@@ -165,6 +167,25 @@ router.get("/price", function(req, res){
   });
 });
 
+router.post("/price/getprice/:origin/:destination", function(req, res){
+  console.log('Origin' + req.params.origin);
+  console.log('Destin' + req.params.destination);
+
+  Location.getLocationByName(req.params.origin, function(cbOri){
+    Location.getLocationByName(req.params.destination, function(cbDest){
+      if (cbOri && cbDest){
+        Price.getPriceByOriginAndDestination(cbOri.locationid, cbDest.locationid, function(cbPri){
+          console.log(cbPri[0]);
+          res.send(cbPri[0]);
+        });
+
+      } else {
+        res.status('204').send('No Match');
+      }
+    });
+  });
+});
+
 router.post("/price", function(req, res){
   console.log("PRICE: POST");
 	console.log(req.body);
@@ -176,7 +197,6 @@ router.post("/price", function(req, res){
   console.log(err);
   if (err.length) {
     Location.getAllLocations(function(cb){
-      console.log(cb);
       res.render('updPrice', {error: err, locations: cb});
     });
     // res.render('updPrice', {error: err});
@@ -184,72 +204,64 @@ router.post("/price", function(req, res){
     // this means that there is nothing wrong, so we can be do the actual work
     var ori, dest;
     console.log('getting origin');
-    Location.getLocationByName(req.body.ori, function(location){
-      var ori = location;
+    Location.getLocationByName(req.body.ori, function(cbOrigin){
+      var ori = cbOrigin;
       console.log(ori);
       console.log('getting destination');
-      Location.getLocationByName(req.body.dest, function(location){
-        var dest = location;
+      Location.getLocationByName(req.body.dest, function(cbDest){
+        var dest = cbDest;
         console.log(dest);
         if (!ori){
           console.log('Origin: {' + req.body.ori +'} not in database, aborting');
         } else if (!dest){
           console.log('Destination: {' + req.body.dest +'} not in database, aborting');
         } else {
-          console.log("We hav enough information to add/update a customer price");
+          console.log("We have enough information to add/update a customer price");
           var price; // do these return an object?
-          // getPriceByOriginAndDestination(ori, dest, function(priceCB){
-            // var price = priceCB;
-            console.log(price);
-            if (price){
-              // updateCustomerPrice(price,
-              //                    {origin: ori,
-              //                     destination: dest,
-              //                     weightcost: req.body.wgt,
-              //                     volumecost: req.body.vol,
-              //                     priority: req.pri},
-              //                     function(callback){ });
+          Price.getPriceByOriginAndDestination(ori.locationid, dest.locationid, function(priceCB){
+            var price = priceCB;
+            console.log('Price: ');
+            console.log(priceCB);
+            console.log(priceCB==false);
+            if (priceCB==false){
+              console.log('insert');
+              Price.insertCustomerPrice({origin: ori.locationid,
+                                  destination: dest.locationid,
+                                  weightcost: req.body.wgt,
+                                  volumecost: req.body.vol,
+                                  priority: req.pri},
+                                  function(callback){ });
             } else {
-              // insertCustomerPrice({origin: ori,
-              //                     destination: dest,
-              //                     weightcost: req.body.wgt,
-              //                     volumecost: req.body.vol,
-              //                     priority: req.pri},
-              //                     function(callback){ });
-
+              console.log('update');
+              Price.updateCustomerPrice(priceCB.priceid,
+                                 {origin: ori.locationid,
+                                  destination: dest.locationid,
+                                  weightcost: req.body.wgt,
+                                  volumecost: req.body.vol,
+                                  priority: req.pri},
+                                  function(callback){ });
             }
             // do stuff for log file?
-            Location.getAllLocations(function(cb){
-                console.log(cb);
-                res.render('updPrice', {locations: cb});
-            });
-            return;
-          // })
+            // Location.getAllLocations(function(cb){
+            //     console.log(cb);
+            //     res.render('updPrice', {locations: cb});
+            // });
+            // return;
+          });
           // turn priority into something usefull
         }
-      })
-    })
+      });
+    });
     // we want to do something if ori and dest have no value
   	Location.getAllLocations(function(cb){
-        console.log(cb);
+        // console.log(cb);
         res.render('updPrice', {locations: cb});
     });
   }
 });
 
 router.get("/cost", function(req, res){
-	console.log('COST');
-	// console.log('Origin: ' + req.query.ori);
-	// console.log('Destin: ' + req.query.dest);
-	// console.log('Compan: ' + req.query.cmpy);
-	// console.log('Type  : ' + req.query.pri);
-	// console.log('Weight: ' + req.query.wgt);
-	// console.log('Volume: ' + req.query.vol);
-	// console.log('Day   : ' + req.query.day);
-	// console.log('Freque: ' + req.query.freq);
-	// console.log('Durati: ' + req.query.dur);
-	// verify then do database stuff?
-	// res.render('updCost', {});
+	console.log('COST: GET');
   Location.getAllLocations(function(cbLoc){
     Company.getAllCompanies(function(cbComp){
       console.log(cbLoc);
@@ -259,15 +271,163 @@ router.get("/cost", function(req, res){
   });
 });
 
-router.post("/cost", function(req, res){
-	console.log(req.body);
-	Location.getAllLocations(function(cbLoc){
-    Company.getAllCompanies(function(cbComp){
-      console.log(cbLoc);
-      console.log(cbComp);
-      res.render('updCost', {locations: cbLoc, companies: cbComp});
-    })
+router.post("/cost/getcost/:origin/:destination", function(req, res){
+  console.log('Origin' + req.params.origin);
+  console.log('Destin' + req.params.destination);
+
+  Location.getLocationByName(req.params.origin, function(cbOri){
+    Location.getLocationByName(req.params.destination, function(cbDest){
+      if (cbOri && cbDest){
+        Route.getRouteByOriginAndDestination(cbOri.locationid, cbDest.locationid, function(cbRou){
+          console.log(cbRou);
+          res.send(cbRou);
+        });
+
+      } else {
+        res.status('204').send('No Match');
+      }
+    });
   });
+});
+
+router.post("/cost", function(req, res){
+  console.log("COST: POST");
+	console.log(req.body);
+
+  // verify input
+  var err = []
+  if (!req.body.ori) {err.push('Origin cannot be Blank.');}
+  if (!req.body.dest) {err.push('Destination cannot be Blank.');}
+  if (!req.body.wgt) {err.push('Weight Price cannot be Blank.');}
+  if (!req.body.vol) {err.push('Volume Price cannot be Blank.');}
+  if (!req.body.freq) {err.push('Frequency cannot be Blank.');}
+  if (!req.body.dur) {err.push('Duration cannot be Blank.');}
+  if (!req.body.mwgt) {err.push('Weight Limit cannot be Blank.');}
+  if (!req.body.mvol) {err.push('Volume Limit cannot be Blank.');}
+  if (err.length){
+    Location.getAllLocations(function(cbLoc){
+      Company.getAllCompanies(function(cbComp){
+        console.log('COST: POST: Error: ' + err);
+        res.render('updCost', {error: err, locations: cbLoc, companies: cbComp});
+        return;
+      })
+    });
+  } else {
+  
+    // check if the locations exist, if not then add them
+    Location.getAllLocations(function(cbLoc){
+      var origin = cbLoc.find(function(find){
+        return find.name.toLowerCase() === req.body.ori.toLowerCase();
+      });
+      if (!origin){
+        Location.insertLocation({name: req.body.ori}, function(cb){
+        console.log(req.body.ori + ' not found in Location database, inserting');
+          console.log(cb);
+        });
+      }
+      var destination = cbLoc.find(function(find){
+        return find.name.toLowerCase() === req.body.dest.toLowerCase();
+      });
+      if (!destination){
+        Location.insertLocation({name: req.body.dest}, function(cb){
+        console.log(req.body.dest + ' not found in Location database, inserting');
+          console.log(cb);
+        });
+      }
+    });
+    // check if the company exists, if not then add it
+    Company.getCompanyByNameAndType(req.body.cmpy, req.body.pri, function(cbCompany){
+      if (!cbCompany){
+        Company.insertCompany({name: req.body.cmpy, type: req.body.pri}, function(callback){
+          if (callback){
+            console.log(callback);
+          }
+        });
+      }
+    });
+
+    // then get them again, because callbacks
+    // but this time we are going to trust that they exist....
+    Location.getAllLocations(function(cbLoc){
+      var origin = cbLoc.find(function(find){
+        return find.name.toLowerCase() === req.body.ori.toLowerCase();
+      });
+      console.log('Origin: ')
+      console.log(origin);
+      var destination = cbLoc.find(function(find){
+        return find.name.toLowerCase() === req.body.dest.toLowerCase();
+      });
+      console.log('Destination: ')
+      console.log(destination);
+      // next we get the company
+      Company.getCompanyByNameAndType(req.body.cmpy, req.body.pri, function(cbCompany){
+        if (!cbCompany){
+          // no company so we are screwed?
+          console.log("There was no company, aborting");
+          return;
+        } else {
+          console.log(cbCompany);
+          // now we get the route
+          // or we would if we had access to the route db...
+          Route.getRouteByOriginAndDestination(origin.locationid, destination.locationid, function(cbRoute){
+            console.log('Route: ');
+            console.log(cbRoute);
+            var route = cbRoute.find(function(find){
+              return find.type == req.body.pri && find.name == req.body.cmpy;
+            });
+            console.log(route);
+            if (!route){
+              // no route exists, so create one (this will probably happen if neither location existed)
+              console.log('insert');
+              Route.insertRoute({company: cbCompany.companyid,
+                                origin: origin.locationid,
+                                destination: destination.locationid,
+                                type: req.body.pri,
+                                weightcost: req.body.wgt,
+                                volumecost: req.body.vol,
+                                maxweight: req.body.mwgt,
+                                maxvolume: req.body.mvol,
+                                duration: req.body.dur,
+                                frequency: req.body.freq,
+                                day: req.body.day}, function(cbInsRoute){
+                                  console.log(cbInsRoute);
+                                });
+            } else {
+              console.log('update');
+              Route.updateRoute(route.routeid, {company: cbCompany.id,
+                                origin: origin.id,
+                                destination: destination.id,
+                                type: req.body.pri,
+                                weightcost: req.body.wgt,
+                                volumecost: req.body.vol,
+                                maxweight: req.body.mwgt,
+                                maxvolume: req.body.mvol,
+                                duration: req.body.dur,
+                                frequency: req.body.freq,
+                                day: req.body.day}, function(cbInsRoute){
+                                  console.log(cbInsRoute);
+                                });
+            }
+            // Route.getAllRoutes(function(cb){
+            //   console.log(cb);
+            // });
+          });
+        }
+        
+      });
+  
+  
+    });
+  
+  
+    Location.getAllLocations(function(cbLoc){
+      Company.getAllCompanies(function(cbComp){
+        // console.log(cbLoc);
+        // console.log(cbComp);
+        res.render('updCost', {locations: cbLoc, companies: cbComp});
+      })
+    });
+  }
 })
 
 // Use the router routes in our application
