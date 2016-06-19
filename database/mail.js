8346a477b69cfcabb6f6d61ab84df362352c576a
thomsedavi <thomsedavi@yahoo.co.nz>
2016-06-19 14:59:28 +1200

    correcting logic for critical routes

--- database/mail.js
+++ database/mail.js
@@ -192,11 +192,9 @@ var Mail = function (dbFile) {
                                     expenses += fullMailAmount[i].destinations[j].priorities[k][l].totalbusinesscost;
                                 }
 
-                                var difference = (expenses)/count;
+                                var difference = (income-expenses)/count;
 
-                                console.log(difference);
-
-                                if (difference > 0) {
+                                if (difference < 0) {
                                     criticalRoutes.push({
                                         origin: fullMailAmount[i].origin,
                                         destination: fullMailAmount[i].destinations[j].destination,
